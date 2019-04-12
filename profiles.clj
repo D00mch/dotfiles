64ee1b97df8e98108bddfcd3b7d1a5d803645b49
liverm0r <arturdumchev@yandex.ru>
2019-04-12 14:29:58 +0300

    push local profiles.clj, configured for emacs cider

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,3 @@
-{:user {:plugins [[jonase/eastwood "0.3.4"]]}}
+{:dev {:plugins      [[cider/cider-nrepl "0.21.1"]
+                      [refactor-nrepl "2.4.0"]]
+       :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
