42f10673a1021f8a422fc1de15fa04d51e9cdd80
Liverm0r <arturdumchev@gmail.com>
2020-07-14 23:02:56 +0300

    reuse lib for desktop manipulations

--- clj_scripts/trutenko/src/trutenko/core.clj
+++ clj_scripts/trutenko/src/trutenko/core.clj
@@ -1,48 +1,27 @@
 (ns trutenko.core
   (:gen-class)
   (:require [clojure.string :as str]
-            [clojure.java.shell :use [sh]])
-  (:import (java.awt ^Robot Robot ^Toolkit Toolkit)
-           (java.awt.datatransfer ^Clipboard Clipboard ^StringSelection StringSelection)
-           (java.awt.event KeyEvent)))
+            [robot.core :as r]
+            [clojure.java.shell :use [sh]]))
 
 (defn open-app! [app-name]
   (sh "sh" "-c" (str "open -a " app-name)))
 
-(def ^Robot robot (Robot.))
-(def ^Clipboard clipboard (.. Toolkit getDefaultToolkit getSystemClipboard))
-
-(defn robot-hot-keys! [keys]
-  (doseq [key keys]
-    (doto ^Robot robot
-      (.keyPress key)
-      (.delay 10)))
-  (.delay ^Robot robot 100)
-  (doseq [key (reverse keys)] (.keyRelease ^Robot robot key)))
-
-(defn robot-put-into-clipboard [^String s]
-  (.setContents clipboard (StringSelection. ^String s) nil))
-
-(defn robot-type! [^Integer i]
-  (doto ^Robot robot
-    (.delay 40)
-    (.keyPress i)
-    (.keyRelease i)))
-
 (defn notify! []
   (open-app! "slack")
-  (.delay ^Robot robot 300)
-  (robot-hot-keys! [KeyEvent/VK_META KeyEvent/VK_SHIFT KeyEvent/VK_K])
-  (.delay ^Robot robot 300)
-  (robot-put-into-clipboard "Trutenko")
-  (robot-hot-keys! [KeyEvent/VK_META KeyEvent/VK_V])
-  (.delay ^Robot robot 400)
-  (robot-type! KeyEvent/VK_ENTER)
-  (.delay ^Robot robot 400)
-  (robot-put-into-clipboard "Привет, это робот Артура. Напоминаю тебе про стендап #staros. Хорошего дня!")
-  (robot-hot-keys! [KeyEvent/VK_META KeyEvent/VK_V])
-  (.delay ^Robot robot 200)
-  (robot-type! KeyEvent/VK_ENTER))
+  (r/delay 300)
+  (r/hot-keys! [:cmd :shift :k])
+  (r/delay 500)
+  (r/clipboard-put "Trutenko")
+  (r/hot-keys! [:cmd :v])
+  (r/delay 700)
+  (r/type! :enter)
+  (r/type! :enter)
+  (r/delay 300)
+  (r/clipboard-put "Привет, это робот Артура. Напоминаю тебе про стендап #staros. Хорошего дня!")
+  (r/hot-keys! [:cmd :v])
+  (r/delay 200)
+  (r/type! :enter))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -50,3 +29,4 @@
   (println "about to notify Trutenko")
   (notify!)
   (System/exit 0))
+
