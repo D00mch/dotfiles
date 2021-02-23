6022887b25e6433e990a99ba9e2647f39c4bf8f9
Liverm0r <arturdumchev@gmail.com>
2021-02-24 00:23:51 +0300

    update nrepl version

--- profiles.clj
+++ profiles.clj
@@ -1,3 +1,3 @@
-{:dev {:plugins      [[cider/cider-nrepl "0.21.1"]
-                      [refactor-nrepl "2.4.0"]]
+{:dev {:plugins      [[cider/cider-nrepl "0.25.9"]
+                      [refactor-nrepl "2.5.1"]]
        :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}}
