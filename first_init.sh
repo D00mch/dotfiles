3c7a1e298ec648126eaaca98155386a6b0ea79be
liverm0r <arturdumchev@yandex.ru>
2022-03-18 13:39:59 +0300

    update initializing script: extend automation

--- first_init.sh
+++ first_init.sh
@@ -2,13 +2,26 @@
 git submodule init
 git submodule update
 
+# install oh my zsh
+cd ~
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# prepare dotfiles (after installing zsh) 
+bash init.sh
+
 # installing brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# installing fzf
+# installing utilities 
 brew install fzf
+brew install neovim
+brew install koekeishiya/formulae/skhd
+# https://github.com/koekeishiya/yabai/wiki/Installing-yabai-(from-HEAD)
+brew install yabai
+brew install the_silver_searcher # ag
+brew install --cask karabiner-elements
 
-# installing ag
-brew install the_silver_searcher
+# start services
+brew services start skhd
+brew services start yabai
 
-bash init.sh
