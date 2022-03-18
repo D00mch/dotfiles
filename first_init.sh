0c38e546f15dd9de4b77800709c71db2ef847bd3
liverm0r <arturdumchev@yandex.ru>
2022-03-18 20:29:13 +0300

    own Cellar

--- first_init.sh
+++ first_init.sh
@@ -12,6 +12,8 @@ bash init.sh
 # installing brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+sudo chown -R $(whoami) /usr/local/Cellar
+
 # installing utilities 
 brew install fzf
 brew install neovim
