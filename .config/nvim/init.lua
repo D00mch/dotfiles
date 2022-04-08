9ff1199823487a35a877213431e27c768229f85f
liverm0r <arturdumchev@yandex.ru>
2022-04-08 23:37:47 +0300

    rm unnecessary rg settings (rg is used from telescope anyway, it has it's own settings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,11 +34,5 @@ set runtimepath^=~/.vim
 let &packpath=&runtimepath
 source ~/.vimrc
 au TextYankPost * silent! lua vim.highlight.on_yank()
-
-" Rg hidden
-command! -bang -nargs=* Rg
-  \ call fzf#vim#grep(
-  \   'rg --hidden --column --line-number --no-heading --color=always --smart-case -- '.shellescape(<q-args>), 1,
-  \   fzf#vim#with_preview(), <bang>0)
 ]])
 
