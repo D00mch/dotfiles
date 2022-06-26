275c294f0382d4246a26670c67cdea655fb6adfe
liverm0r <arturdumchev@yandex.ru>
2022-06-27 01:50:35 +0300

    update compile script and gitignore

--- clj_scripts/theme/compile.sh
+++ clj_scripts/theme/compile.sh
@@ -8,13 +8,15 @@ echo '[
   }
   ]' | tee META-INF/native-image/logging.json
 
-# remove -H:+StaticExecutableWithDynamicLibC for m1 builds
+# add -H:+StaticExecutableWithDynamicLibC for linux buids
+# -Djava.awt.headless=false needed for robot, https://github.com/oracle/graal/issues/3064
 
 native-image -jar target/uberjar/theme-0.1.0-SNAPSHOT-standalone.jar \
-    -H:Name=theme --initialize-at-build-time --no-fallback \
+    --initialize-at-build-time -H:Name=theme --no-fallback \
     -J-Dclojure.spec.skip.macros=true -J-Dclojure.compiler.direct-linking=true \
     --verbose --no-server -J-Xmx3G \
     --report-unsupported-elements-at-runtime --native-image-info \
+    -Djava.awt.headless=false \
     -H:CCompilerOption=-pipe \
     -H:ReflectionConfigurationFiles=META-INF/native-image/logging.json \
     -H:+ReportExceptionStackTraces
--- clj_scripts/theme/project.clj
+++ clj_scripts/theme/project.clj
@@ -5,6 +5,6 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.0"]
                  [robot "0.2.1-SNAPSHOT"]]
-  :main ^:skip-aot theme.core
+  :main theme.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
