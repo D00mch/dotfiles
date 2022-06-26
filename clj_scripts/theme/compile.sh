d9936d1e5c0f96b7c0c4df7b514f843b76cf4be8
liverm0r <arturdumchev@yandex.ru>
2022-06-26 22:46:37 +0300

    add theme complie to graalvm; update gitignore

---
+++ clj_scripts/theme/compile.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+mkdir -p META-INF/native-image
+
+echo '[
+  { 
+    "name": "java.lang.reflect.AccessibleObject",
+    "methods" : [{"name":"canAccess"}]
+  }
+  ]' | tee META-INF/native-image/logging.json
+
+# remove -H:+StaticExecutableWithDynamicLibC for m1 builds
+
+native-image -jar target/uberjar/theme-0.1.0-SNAPSHOT-standalone.jar \
+    -H:Name=theme --initialize-at-build-time --no-fallback \
+    -J-Dclojure.spec.skip.macros=true -J-Dclojure.compiler.direct-linking=true \
+    --verbose --no-server -J-Xmx3G \
+    --report-unsupported-elements-at-runtime --native-image-info \
+    -H:CCompilerOption=-pipe \
+    -H:ReflectionConfigurationFiles=META-INF/native-image/logging.json \
+    -H:+ReportExceptionStackTraces
+
+chmod +x theme
