86b4706ce90938b4dc56fb101deb55b6bf73f065
liverm0r <arturdumchev@yandex.ru>
2023-07-08 00:46:11 +0300

    Update Wezterm UI
    - use blur instead of paralax;
    - don't show tabs when one item;
    - don't show tabs numbers;

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,60 +6,7 @@ local wezterm = require 'wezterm'
 
 local dimmer = { brightness = 0.05 }
 local home = wezterm.home_dir .. '/dotfiles/resources/'
-local paralax_bg = {
-  {
-    source = {
-      File = home..'Alien_Ship_bg_vert_images/Backgrounds/spaceship_bg_1@2x.png',
-    },
-    repeat_x = 'Mirror',
-    hsb = dimmer,
-    attachment = { Parallax = 0.1 },
-  },
-  {
-    source = {
-      File = home .. 'Alien_Ship_bg_vert_images/Overlays/overlay_1_spines@2x.png',
-    },
-    width = '100%',
-    repeat_x = 'NoRepeat',
-    vertical_align = 'Bottom',
-    repeat_y_size = '200%',
-    hsb = dimmer,
-    attachment = { Parallax = 0.2 },
-  },
-  {
-    source = {
-      File = home .. 'Alien_Ship_bg_vert_images/Overlays/overlay_2_alienball@2x.png',
-    },
-    width = '100%',
-    repeat_x = 'NoRepeat',
-    vertical_offset = '10%',
-    repeat_y_size = '200%',
-    hsb = dimmer,
-    attachment = { Parallax = 0.3 },
-  },
-  {
-    source = {
-      File = home .. 'Alien_Ship_bg_vert_images/Overlays/overlay_3_lobster@2x.png',
-    },
-    width = '100%',
-    repeat_x = 'NoRepeat',
-    vertical_offset = '30%',
-    repeat_y_size = '200%',
-    hsb = dimmer,
-    attachment = { Parallax = 0.4 },
-  },
-  {
-    source = {
-      File = home .. 'Alien_Ship_bg_vert_images/Overlays/overlay_4_spiderlegs@2x.png',
-    },
-    width = '100%',
-    repeat_x = 'NoRepeat',
-    vertical_offset = '50%',
-    repeat_y_size = '150%',
-    hsb = dimmer,
-    attachment = { Parallax = 0.5 },
-  },
-}
+
 
 function scheme_for_appearance(appearance)
   if appearance:find("Dark") then
@@ -69,21 +16,12 @@ function scheme_for_appearance(appearance)
   end
 end
 
-function bg_scheme(appearance)
-  if appearance:find("Dark") then
-    return paralax_bg
-  else
-    return {}
-  end
-end
-
 wezterm.on("window-config-reloaded", function(window, pane)
   local overrides = window:get_config_overrides() or {}
   local appearance = window:get_appearance()
   local scheme = scheme_for_appearance(appearance)
   if overrides.color_scheme ~= scheme then
     overrides.color_scheme = scheme
-    overrides.background = bg_scheme(appearance)
     window:set_config_overrides(overrides)
   end
 end)
@@ -355,12 +293,19 @@ local key_tables = {
 --
 
 local config = {
+  window_background_opacity = 0.85,
+  macos_window_background_blur = 20,
+
   font = wezterm.font('Hack Nerd Font Mono', { weight = 'Bold' }),
   font_size = 14,
   keys = keys,
   key_tables = key_tables,
   enable_scroll_bar = true,
   min_scroll_bar_height = '2cell',
+
+  -- Tab Bar
+  hide_tab_bar_if_only_one_tab = true,
+  show_tab_index_in_tab_bar = false,
 }
 
 return config
