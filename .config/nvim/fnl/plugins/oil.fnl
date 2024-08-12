316f7f45e006738477ad8d27e73fc35361d3e2b6
dumch <arturdumchev@yandex.ru>
2024-08-13 00:14:03 +0300

    Update oil config

--- .config/nvim/fnl/plugins/oil.fnl
+++ .config/nvim/fnl/plugins/oil.fnl
@@ -6,9 +6,11 @@
   :config (fn []
             (let [oil (require :oil)
                   actions (require :oil.actions)]
+              (vim.api.nvim_command "set splitright")
               (oil.setup
                {:keymaps
                 {:th actions.toggle_hidden
                  :<Tab> actions.preview
-                 :<Cr> actions.select_vsplit
-                 :<S-Cr> actions.select}})))}]
+                 :<Cr> actions.select
+                 :<S-Cr> actions.select_vsplit
+                 }})))}]
--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,14 @@
--- [nfnl] Compiled from .config/nvim/fnl/plugins/oil.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] Compiled from fnl/plugins/oil.fnl by https://github.com/Olical/nfnl, do not edit.
 local _local_1_ = require("config.util")
 local kset = _local_1_["kset"]
 local function _2_()
-  return (require("oil")).open()
+  return require("oil").open()
 end
 kset("n", "<Space>ff", _2_)
 local function _3_()
   local oil = require("oil")
   local actions = require("oil.actions")
-  return oil.setup({keymaps = {th = actions.toggle_hidden, ["<Tab>"] = actions.preview, ["<Cr>"] = actions.select_vsplit, ["<S-Cr>"] = actions.select}})
+  vim.api.nvim_command("set splitright")
+  return oil.setup({keymaps = {th = actions.toggle_hidden, ["<Tab>"] = actions.preview, ["<Cr>"] = actions.select, ["<S-Cr>"] = actions.select_vsplit}})
 end
 return {{"stevearc/oil.nvim", lazy = true, config = _3_}}
