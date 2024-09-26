cb5bc695aade1fb2727d7a3f968515fdd487e39a
dumch <arturdumchev@yandex.ru>
2024-09-27 02:32:53 +0300

    Update gpt/alfred workflow script

--- scripts/gpt.bash
+++ scripts/gpt.bash
@@ -6,9 +6,18 @@ source ~/.zprofile
 WIFI_NAME="asus-vr"
 WIFI_PASSWORD="$WIFI_HOME"
 
-networksetup -setairportnetwork en0 "$WIFI_NAME" "$WIFI_PASSWORD"
+get_current_ssid() {
+    networksetup -getairportnetwork en0 | awk -F': ' '{print $2}'
+}
 
-CURRENT_SSID=$(networksetup -getairportnetwork en0 | awk -F': ' '{print $2}')
+CURRENT_SSID=$(get_current_ssid)
+
+# Check if the Wi-Fi network has been changed
+if [ "$CURRENT_SSID" != "$WIFI_NAME" ]; then
+    echo "About to switch network"
+    networksetup -setairportnetwork en0 "$WIFI_NAME" "$WIFI_PASSWORD"
+    CURRENT_SSID=$(get_current_ssid)
+fi
 
 if [ "$CURRENT_SSID" == "$WIFI_NAME" ]; then
     echo "Successfully connected to $WIFI_NAME."
