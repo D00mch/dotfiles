afaff4f73f7751f3a4f04765305fba944cc2c22b
dumch <arturdumchev@yandex.ru>
2024-10-02 23:23:11 +0300

    add new bindngs to insert := in go files

--- .config/nvim/ftplugin/go.fnl
+++ .config/nvim/ftplugin/go.fnl
@@ -1,7 +1,7 @@
 (local {: autoload} (require :nfnl.module))
 (local {: bkset} (autoload :config.util))
 
-(bkset :i :<D-e> ":=")
+(bkset :i "<D-;>" ":=")
 (bkset :n :<Leader>tc ":GoTestFunc -v -F<Cr>")
 (bkset :n :<Leader>tb ":GoTestFile -v -F<Cr>")
 (bkset :n :<Leader>ta ":GoTest -v -F<Cr>")
--- .config/nvim/ftplugin/go.lua
+++ .config/nvim/ftplugin/go.lua
@@ -3,7 +3,7 @@ local _local_1_ = require("nfnl.module")
 local autoload = _local_1_["autoload"]
 local _local_2_ = autoload("config.util")
 local bkset = _local_2_["bkset"]
-bkset("i", "<D-e>", ":=")
+bkset("i", "<D-;>", ":=")
 bkset("n", "<Leader>tc", ":GoTestFunc -v -F<Cr>")
 bkset("n", "<Leader>tb", ":GoTestFile -v -F<Cr>")
 bkset("n", "<Leader>ta", ":GoTest -v -F<Cr>")
