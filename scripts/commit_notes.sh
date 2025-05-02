a70ee7945bf996cc5a724cb9485cbf4a19b18c95
dumch <arturdumchev@yandex.ru>
2025-05-02 17:58:37 +0300

    Update notes with crone job

---
+++ scripts/commit_notes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NOTES_DIR="$HOME/Yandex.Disk.localized/wiki/"
+
+cd "$NOTES_DIR" || { echo "Directory not found: $NOTES_DIR"; exit 1; }
+
+git add .
+
+git commit -m "Daily update: $(date +'%Y-%m-%d %H:%M:%S')"
+
+git push origin main
+
+# 0 12 * * * /Users/m1/dotfiles/scripts/commit_notes.sh >> /Users/m1/Documents/chone.log 2>&1
