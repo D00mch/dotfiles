679376abcfb5f1a431bb3b4db6b149f6846db447
dumch <arturdumchev@gmail.com>
2025-10-06 18:28:53 +0300

    Get nvim-rest back

---
+++ .config/nvim/lua/plugins/rest.lua
@@ -0,0 +1,14 @@
+-- [nfnl] fnl/plugins/rest.fnl
+local _local_1_ = require("nfnl.module")
+local autoload = _local_1_["autoload"]
+local _local_2_ = autoload("config.util")
+local bkset = _local_2_["bkset"]
+local function setup_rest()
+  bkset("n", "<Leader>a", "<Plug>RestNvimPreview")
+  bkset("n", "<Leader>e", "<Plug>RestNvim")
+  return bkset("n", "<Leader>q", "<Plug>RestNvim")
+end
+local function _3_()
+  return vim.api.nvim_create_autocmd("BufWinEnter", {pattern = "*.http", group = vim.api.nvim_create_augroup("HttpMappings", {clear = true}), callback = setup_rest})
+end
+return {{"rest-nvim/rest.nvim", lazy = true, ft = {"http"}, init = _3_, opts = {result_split_in_place = true, jump_to_request = true}, config = true}}
