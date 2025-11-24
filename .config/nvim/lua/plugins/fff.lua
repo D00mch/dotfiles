85050dd21ed8741b114f043b393524f2f3946815
dumch <arturdumchev@gmail.com>
2025-11-24 14:33:52 +0300

    RM fff

--- .config/nvim/lua/plugins/fff.lua
+++
@@ -1,8 +0,0 @@
--- [nfnl] fnl/plugins/fff.fnl
-local function _1_()
-  return require("fff.download").download_or_build_binary()
-end
-local function _2_()
-  return require("fff").find_in_git_root()
-end
-return {"dmtrKovalenko/fff.nvim", build = _1_, keys = {{"ff", _2_, desc = "FFFind files"}}, opts = {debug = {enabled = true, show_scores = true}}, lazy = false}
