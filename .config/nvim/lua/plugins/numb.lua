14bee0fe053178708a0be643587b1df61353f2f4
dumch <arturdumchev@yandex.ru>
2026-09-08 20:19:22 +0300

    Remove orphaned lua files

--- .config/nvim/lua/plugins/numb.lua
+++
@@ -1,2 +0,0 @@
--- [nfnl] Compiled from fnl/plugins/numb.fnl by https://github.com/Olical/nfnl, do not edit.
-return {{"nacro90/numb.nvim"}}
--- .config/nvim/lua/plugins/ssr.lua
+++
@@ -1,8 +0,0 @@
--- [nfnl] Compiled from .config/nvim/fnl/plugins/ssr.fnl by https://github.com/Olical/nfnl, do not edit.
-local function _1_()
-  local ssr = require("ssr")
-  local _let_2_ = require("config.util")
-  local kset = _let_2_["kset"]
-  return kset({"n", "x"}, "<Leader>sr", ssr.open, "Search/Replace")
-end
-return {{"cshuaimin/ssr.nvim", lazy = true, init = _1_, opts = {keymaps = {close = "<D-w>", next_match = "n", prev_match = "N", replace_confirm = "<CR>", replace_all = "<Leader><Cr>"}}, config = true}}
