17adacd1b318a406d1b57060a35920c37cd9adee
dumch <arturdumchev@yandex.ru>
2026-09-25 01:43:46 +0300

    Update <cmd>b window height

--- .config/nvim/fnl/plugins/lsp_glance.fnl
+++ .config/nvim/fnl/plugins/lsp_glance.fnl
@@ -7,7 +7,9 @@
             (kset :n :<D-b> "mZg*`Z:Glance references<Cr>" {:desc "Show refs (Idea)"})))
   :config
   (fn []
-    (let [glance (require :glance)]
+    (let [glance (require :glance)
+          config (require :glance.config)
+          resize-preview #(set config.options.height (math.max 1 (math.floor (* vim.o.lines 0.7))))]
       (glance.setup
         {:mappings
          {:list {:gh (glance.actions.enter_win :preview)
@@ -17,5 +19,8 @@
           :preview {:gl (glance.actions.enter_win :list)
                     ;:<D-t> glance.actions.jump_tab
                     }}
-         :border {:enable true}
-         :height 25})))}]
+         :border {:enable true}})
+      (resize-preview)
+      (vim.api.nvim_create_autocmd :VimResized
+        {:group (vim.api.nvim_create_augroup :GlancePreviewHeight {:clear true})
+         :callback resize-preview})))}]
--- .config/nvim/lua/plugins/lsp_glance.lua
+++ .config/nvim/lua/plugins/lsp_glance.lua
@@ -1,11 +1,20 @@
--- [nfnl] Compiled from fnl/plugins/lsp_glance.fnl by https://github.com/Olical/nfnl, do not edit.
+-- [nfnl] fnl/plugins/lsp_glance.fnl
 local function _1_()
   local _let_2_ = require("config.util")
-  local kset = _let_2_["kset"]
+  local kset = _let_2_.kset
   return kset("n", "<D-b>", "mZg*`Z:Glance references<Cr>", {desc = "Show refs (Idea)"})
 end
 local function _3_()
   local glance = require("glance")
-  return glance.setup({mappings = {list = {gh = glance.actions.enter_win("preview"), ["<left>"] = glance.actions.preview_scroll_win(5), ["<right>"] = glance.actions.preview_scroll_win(-5)}, preview = {gl = glance.actions.enter_win("list")}}, border = {enable = true}, height = 25})
+  local config = require("glance.config")
+  local resize_preview
+  local function _4_()
+    config.options.height = math.max(1, math.floor((vim.o.lines * 0.7)))
+    return nil
+  end
+  resize_preview = _4_
+  glance.setup({mappings = {list = {gh = glance.actions.enter_win("preview"), ["<left>"] = glance.actions.preview_scroll_win(5), ["<right>"] = glance.actions.preview_scroll_win(-5)}, preview = {gl = glance.actions.enter_win("list")}}, border = {enable = true}})
+  resize_preview()
+  return vim.api.nvim_create_autocmd("VimResized", {group = vim.api.nvim_create_augroup("GlancePreviewHeight", {clear = true}), callback = resize_preview})
 end
 return {{"DNLHC/glance.nvim", lazy = true, ft = {"clojure", "go", "dart", "markdown", "md"}, cmd = {"LspInfo", "LspInstall", "LspUninstall"}, init = _1_, config = _3_}}
